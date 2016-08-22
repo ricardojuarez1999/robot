78039a8ce07aedc10b7246c0853c90c332b78492
ricardojuarez1999 <ricardojuarez1999@gmail.com>
2016-08-22 14:39:47 -0600

    hola

--- ruplay.py
+++ ruplay.py
@@ -6,3 +6,12 @@ def cargar_mapa(nombre):
 	return lista
 mapa = "mapas/mapa1.txt"
 print (cargar_mapa(mapa))
+
+def cargar_instrucciones(nombre):
+	instrucciones = open(nombre, "r")
+	lista_2 = []
+	for linea in instrucciones:
+		lista_2.append(linea.strip())
+	return lista_2
+inst = "instrucciones/instruccion1.txt"
+print (cargar_instrucciones(inst))
